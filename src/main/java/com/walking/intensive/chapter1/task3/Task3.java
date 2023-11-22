606bd8e55f902b6d2381ff7b5d2ef5feb064fe7c
Oleg <kovalyovop@yandex.ru>
2023-11-23 00:42:55 +0300

    Task3 up

---
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -0,0 +1,11 @@
+package com.walking.intensive.chapter1.task3;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean isLeap(int year) {
+        return true;
+    }
+}
