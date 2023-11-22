010553f9beceb5489a73257000e766c3ac2fa8a1
Oleg <kovalyovop@yandex.ru>
2023-11-23 00:39:06 +0300

    Task3 up

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -3,15 +3,24 @@ package com.walking.intensive.chapter1.task3;
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-leap-year.html">ссылка</a>
  */
-public class Main {
+public class Task3 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
 
+        int[] years1 = {400, 1600, 2000, 2020, 2024, 2028, 2032, 2040, 2400};
+        int[] years2 = {1000, 1100, 1300, 1400, 1500, 1700, 1800, 2023, 2100};
+
+        System.out.println("Должно быть true:");
+        for (int year : years1) {
+            System.out.println("Указанный год: " + year + " результат проверки: " + isLeap(year));
+        }
+
+        System.out.println("Должно быть false:");
+        for (int year : years2) {
+            System.out.println("Указанный год: " + year + " результат проверки: " + isLeap(year));
+        }
     }
 
     static boolean isLeap(int year) {
-        //        Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
