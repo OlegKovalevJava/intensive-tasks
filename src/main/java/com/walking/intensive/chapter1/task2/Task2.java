771f9fe062d5446749b502ba5dbcf3859aa22e7c
Oleg <kovalyovop@yandex.ru>
2023-11-27 06:56:07 +0300

    Chapter1Task2+

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -1,23 +1,26 @@
 package com.walking.intensive.chapter1.task2;
-
 /**
  * Условие: <a href="https://geometry-math.ru/homework/Java-house.html">ссылка</a>
  */
 public class Task2 {
     public static void main(String[] args) {
-        String test1 = getFlatLocation(5, 2, 21);
-        String test2 = getFlatLocation(5, 2, 41);
-        System.out.println(test1);
-        System.out.println();
-        System.out.println(test2);
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         final int totalFlatFloor = 4;
-        int totalFlatsEntrance = floorAmount * totalFlatFloor; // итого квартир в подъезде
+        int totalFlatsEntrance = floorAmount * totalFlatFloor;
 
         if (flatNumber < 1 || flatNumber > (entranceAmount * totalFlatsEntrance)) {
             return "Такой квартиры не существует";
+        }
+
+        if (floorAmount < 1) {
+            return "Такого этажа не существует";
+        }
+
+        if (entranceAmount < 1) {
+            return "Такого подъезда не существует";
         }
 
         int entranceNumber;
@@ -36,7 +39,7 @@ public class Task2 {
             floorNumber = (flatNumber / totalFlatFloor + 1) - floorAmount * (entranceNumber - 1);
         }
 
-        String flatLocation = switch (flatNumber % totalFlatFloor) { // расположение относительно лифта
+        String flatLocation = switch (flatNumber % totalFlatFloor) {
             case (0) -> "справа от лифта, вправо";
             case (1) -> "слева от лифта, влево";
             case (2) -> "слева от лифта, вправо";
